b70e7017affd08373a2e5a4e403dc4480e375fc0
AlekRing <okolcov60@gmail.com>
2020-10-03 16:16:28 +0300

    feat: add transform array decision

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,15 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (!Array.isArray(arr) || !arr) return 'THROWN'
+  if (!Array.isArray(arr) || !arr) return  new error('THROWN')
   else if (arr == null || arr == undefined) return 'THROWN'
   let newArr = []
 
   for (let i = 0, chek = 0; i < arr.length; i++, chek++) {
     
     if (i === 0 && (arr[i] === `--double-prev` || arr[i] === '--discard-prev')) { chek-- }
-    else if (i + 1 === arr.length && (arr[i] === `--discard-next` || arr[i] === '--double-next')) { i++ }
-    else if (arr[i] === `--double-prev`) { newArr[chek] = arr[i - 1]
+    else if (i + 1 === arr.length && (arr[i] === `--discard-next` || arr[i] === '--double-next')) i++ 
+    else if (arr[i] === `--double-prev`) newArr[chek] = arr[i - 1]
+    else if (arr[i] === `--discard-next` && arr[i + 2] === '--double-prev') {
+      chek--
+      i += 2
+    }
+    else if (arr[i] === `--discard-next` && arr[i + 2] === '--discard-prev') {
+      chek--
+      i += 2
     }
     else if (arr[i] === `--discard-next`) {
       chek--
@@ -19,6 +26,7 @@ module.exports = function transform(arr) {
       i++
       chek--
     }
+    else if (arr[i] === `--double-next`) newArr[chek] = arr[i + 1]
     else newArr[chek] = arr[i]
   }
   
